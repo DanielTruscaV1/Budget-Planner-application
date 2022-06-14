e74b737c60a60e2cf55aa288a85c9ce2929c519f
DanielTruscaV1 <danieltrusca2016@gmail.com>
2022-06-14 14:38:00 +0300

    Released the second version of the application.

--- src/components/AddExpenseForm.js
+++ src/components/AddExpenseForm.js
@@ -1,31 +1,55 @@
-import React from "react";
+import React, {useState, useContext} from "react";
+import {AppContext} from "../context/AppContext";
+import {v4 as uuidv4} from "uuid";
 
 const AddExpenseForm = () => {
+	const {dispatch} = useContext(AppContext);
+
+	const [name, setName] = useState("");
+	const [cost, setCost] = useState("");
+
+	const onSubmit = event => {
+		event.preventDefault();
+		const expense = {
+			id: uuidv4(),
+			name: name,
+			cost: parseInt(cost)
+		}
+		dispatch({
+			type:"ADD_EXPENSE",
+			payload:expense
+		})
+	}
+
     return (
         <form>
-			<div class='row'>
-				<div class='col-sm col-lg-4'>
-					<label for='name'>Name</label>
+			<div className='row'>
+				<div className='col-sm col-lg-4'>
+					<label htmlFor='name'>Name</label>
 					<input
 						required='required'
 						type='text'
-						class='form-control'
+						className='form-control'
 						id='name'
+						value={name}
+						onChange={(event) => {setName(event.target.value)}}
 					/>
 				</div>
-				<div class='col-sm col-lg-4'>
-					<label for='cost'>Cost</label>
+				<div className='col-sm col-lg-4'>
+					<label htmlFor='cost'>Cost</label>
 					<input
 						required='required'
 						type='number'
-						class='form-control'
+						className='form-control'
 						id='cost'
+						value={cost}
+						onChange={(event) => {setCost(event.target.value)}}
 					/>
 				</div>
 			</div>
-			<div class='row mt-3'>
-				<div class='col-sm'>
-					<button type='submit' class='btn btn-primary'>
+			<div className='row mt-3'>
+				<div className='col-sm'>
+					<button type='submit' className='btn btn-primary' onClick={onSubmit}>
 						Save
 					</button>
 				</div>
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,26 @@
-import React from "react";
-import {TiDelete} from "react-icons/ti";
+import React, { useContext } from 'react';
+import { TiDelete } from 'react-icons/ti';
+import { AppContext } from '../context/AppContext';
 
 const ExpenseItem = (props) => {
-    return (
-        <li className='list-group-item d-flex justify-content-between align-items-center'>
+	const { dispatch } = useContext(AppContext);
+
+	const handleDeleteExpense = () => {
+		dispatch({
+			type: 'DELETE_EXPENSE',
+			payload: props.id,
+		});
+	};
+
+	return (
+		<li class='list-group-item d-flex justify-content-between align-items-center'>
 			{props.name}
 			<div>
-				<span>${props.cost}</span>
-				<TiDelete size='1.5em'/>
+				<span class='badge badge-primary badge-pill mr-3'>£{props.cost}</span>
+				<TiDelete size='1.5em' onClick={handleDeleteExpense} />
 			</div>
 		</li>
-    );
-}
+	);
+};
 
 export default ExpenseItem;
